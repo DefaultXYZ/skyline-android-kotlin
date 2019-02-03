a2f76e6f5d9f327d2ceed81719d1bd7d06d07fd0
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2019-02-03 23:19:29 +0100

    Add Login/Map empty activities to project

---
+++ app/src/main/java/com/defaultxyz/skyline/presentation/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.defaultxyz.skyline.presentation.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.defaultxyz.skyline.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/defaultxyz/skyline/presentation/map/MapActivity.kt
@@ -0,0 +1,13 @@
+package com.defaultxyz.skyline.presentation.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.defaultxyz.skyline.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
